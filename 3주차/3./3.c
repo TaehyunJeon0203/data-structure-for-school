94e76ed2d13059047fa680bf79208d65b6030385
TaehyunJeon <jeontaehyun0203@gmail.com>
2024-09-25 17:46:43 +0900

    과제 3 완료

--- 3주차/3./3.c
+++ 3주차/3./3.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(void) {
-    int N, M;
+int N, M;
+int arr[8];
+bool used[9];
+
+void backtrack(int depth) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (!used[i]) {
+            arr[depth] = i;
+            used[i] = true;
+            backtrack(depth + 1);
+            used[i] = false;
+        }
+    }
+}
+int main() {
     scanf("%d %d", &N, &M);
 
-    
+    backtrack(0);
+
+    return 0; 
 }
